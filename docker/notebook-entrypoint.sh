f4e426487b34315e741ce48af5b2b1a8d9d53eef
Sirly <sirlydreamer@gmail.com>
2023-04-01 10:48:06 +0800

    Ride camel with multiple GPU

--- docker/notebook-entrypoint.sh
+++ docker/notebook-entrypoint.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
+umask 000
 
-WORK_DIR=/workspace/
+WORK_DIR=/workspace
+JUPYTER_CONFIG="$WORK_DIR/.jupyter/jupyter_lab_config.py"
 
-if [ ! -n "$http_proxy" ]; then
-    if [ -n "$PYPI_MIRROR" ]; then
-        pip3 config set global.index-url $PYPI_MIRROR
-    else
-        pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-    fi
-fi
+cd $WORK_DIR
+
+[[ ! -d "$XDG_CACHE_HOME" ]] && mkdir -p $XDG_CACHE_HOME
 
 if [[ ! -f "$HOME/.pip_env.ok" ]]; then
-    pip3 install jupyterlab bitsandbytes datasets loralib sentencepiece &&\
-    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 &&\
-    pip3 install git+https://github.com/zphang/transformers@c3dc391 &&\
-    pip3 install git+https://github.com/huggingface/peft.git &&\
+    set -e
+    if [ -z "$http_proxy" ]; then
+        if [ -n "$PYPI_MIRROR" ]; then
+            pip3 config set global.index-url $PYPI_MIRROR
+        else
+            pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+        fi
+    fi
+    pip3 install jupyterlab bitsandbytes datasets loralib sentencepiece gradio
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+    pip3 install git+https://github.com/huggingface/transformers.git
+    pip3 install git+https://github.com/huggingface/peft.git
+    set +e
     touch $HOME/.pip_env.ok
 fi
 
-if [[ ! -f "$HOME/.jupyter/jupyter_lab_config.py" ]]; then
+if [[ ! -f "$JUPYTER_CONFIG" ]]; then
     jupyter lab --generate-config
 fi
 
